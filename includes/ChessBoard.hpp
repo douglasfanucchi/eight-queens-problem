8a5360847bd08e6417d8f338e39e103d254c1b4f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-25 05:48:53 -0300

    tests: test creates a 2x2 chess board

---
+++ includes/ChessBoard.hpp
@@ -0,0 +1,10 @@
+#ifndef CHESS_BOARD_H
+# define CHESS_BOARD_H
+
+class ChessBoard {
+    public:
+        ChessBoard(int);
+        bool isAvailable(int, int);
+};
+
+#endif
--- tests/unit/ChessBoard.cpp
+++ tests/unit/ChessBoard.cpp
@@ -1,11 +1,17 @@
 #include <asserts.hpp>
+#include <ChessBoard.hpp>
 
-void test_should_assert_true()
+void test_should_create_a_2x2_chess_board()
 {
-    ASSERT_TRUE(true);
+    ChessBoard board(2);
+
+    ASSERT_TRUE(board.isAvailable(0, 0));
+    ASSERT_TRUE(board.isAvailable(0, 1));
+    ASSERT_TRUE(board.isAvailable(1, 0));
+    ASSERT_TRUE(board.isAvailable(1, 1));
 }
 
 void RUN_CHESS_BOARD_TEST_SUITE()
 {
-    test_should_assert_true();
+    test_should_create_a_2x2_chess_board();
 }
