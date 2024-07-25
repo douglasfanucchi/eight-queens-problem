90800e4c1952836c3ced86202a5cb4bf6265bad8
Douglas Fanucchi <douglasfanucchi@gmail.com>
2024-07-25 13:07:51 -0300

    prints all solutions to a 7x7 board

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <ChessBoard.hpp>
 
 int main()
 {
-    std::cout << "hello world";
+    int boardSize = 7;
+    ChessBoard board(boardSize);
+    std::vector<ChessBoard> solutions = board.solveQueensPuzzle(0);
+    if (!solutions.size()) {
+        return 1;
+    }
+    for (int i = 0; i < solutions.size(); i++) {
+        for (int j = 0; j < boardSize; j++) {
+            std::cout << solutions[i].checkQueen(j) << std::endl;
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
